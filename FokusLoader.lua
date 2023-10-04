3c08034206907cb0f42b49bf509bfce12b63d96f
FokusScripts <122217813+FokusScripts@users.noreply.github.com>
2023-10-04 21:06:30 +0100

    FokusLoader.lua

--- FokusLoader.lua
+++ FokusLoader.lua
@@ -1,1 +1,20 @@
---wait
+repeat
+    wait()
+until game:IsLoaded()
+
+local WebSiteLink = "https://raw.githubusercontent.com/FokusScripts/Fokus/main/"
+
+local SupportedGames = {
+    [71315343] = "FokusDBRScript.lua",
+    [4866692557] = "FokusAOHScript.lua",
+	[7503115095] = "FokusPITScript.lua",
+    [14184086618] = "FokusOBYOABScript.lua"
+}
+
+for i, v in pairs(SupportedGames) do
+   if i == game.PlaceId or i == game.GameId then
+      loadstring(game:HttpGet(WebSiteLink..v, true))()
+   end
+end
+
+-- Script Made By Crimson Hub Owner
