699e7c815fb40b81ea405a33c33c00557b0ad4f2
FokusScripts <122217813+FokusScripts@users.noreply.github.com>
2023-10-06 20:10:38 +0100

    FokusLoader.lua

--- FokusLoader.lua
+++ FokusLoader.lua
@@ -7,8 +7,9 @@ local WebSiteLink = "https://raw.githubusercontent.com/FokusScripts/Fokus/main/"
 local SupportedGames = {
     [71315343] = "FokusDBRScript.lua",
     [4866692557] = "FokusAOHScript.lua",
-	[7503115095] = "FokusPITScript.lua",
-    [14184086618] = "FokusOBYOABScript.lua"
+    [7503115095] = "FokusPITScript.lua",
+    [14184086618] = "FokusOBYOABScript.lua",
+    [3851622790] = "FokusBISScript.lua"
 }
 
 for i, v in pairs(SupportedGames) do
