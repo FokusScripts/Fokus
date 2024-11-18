e21a878f1c0b13049fd0ef0f3efba9dda613418b
FokusScripts <122217813+FokusScripts@users.noreply.github.com>
2024-11-18 23:28:09 +0000

    Update FokusLoader.lua

--- FokusLoader.lua
+++ FokusLoader.lua
@@ -5,12 +5,13 @@ until game:IsLoaded()
 local WebSiteLink = "https://raw.githubusercontent.com/FokusScripts/Fokus/main/"
 
 local SupportedGames = {
-    [71315343] = "FokusDBRScript.lua",
-    [7503115095] = "FokusPITScript.lua",
-    [17893531862] = "FokusPITScript.lua",
-    [13083893317] = "FokusLSScript.lua",
-	[192800] = "FokusWAAPPScript.lua",
-    [6403373529] = "FokusSBScript.lua"
+   [71315343] = "FokusDBRScript.lua",
+   [7503115095] = "FokusPITScript.lua",
+   [17893531862] = "FokusPITScript.lua",
+   [13083893317] = "FokusLSScript.lua",
+   [192800] = "FokusWAAPPScript.lua",
+   [6403373529] = "FokusSBScript.lua",
+   [16426795556] = "FokusTFScript.lua"
 }
 
 for i, v in pairs(SupportedGames) do
